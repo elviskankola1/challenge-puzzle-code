f1ec759eeb3d8d306c5a8bdb605326c2ce4ead99
elvis kankola <elviskankola1@gmail>
2018-11-12 19:07:36 +0200

    NEW FUNCTION

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -4,10 +4,18 @@
 
     class Welcome extends CI_Controller {
 
+        private $table_formation = 'mfo_formation';
         function __construct(){
            
             parent::__construct();
         }
-        
+
+        public function add_formation($titre,$description,$file){
+
+            $this->db->set('',$titre);
+            $this->db->set('',$description);
+            $this->db->set('',$file);
+            return $this->db->insert($this->table_formation);
+        }
 
     }
