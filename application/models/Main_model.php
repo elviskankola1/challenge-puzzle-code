b9a190b9ebeec339c76b7684ed79d2252b0b0400
elvis kankola <elviskankola1@gmail>
2018-11-12 19:26:49 +0200

    NEW FUNCTION

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -5,17 +5,47 @@
     class Welcome extends CI_Controller {
 
         private $table_formation = 'mfo_formation';
+        private $table_application = 'mfo_application';
         function __construct(){
            
             parent::__construct();
         }
 
-        public function add_formation($titre,$description,$file){
+        public function Add_Formation($titre,$description,$file){
 
             $this->db->set('',$titre);
             $this->db->set('',$description);
             $this->db->set('',$file);
             return $this->db->insert($this->table_formation);
         }
+
+        public function Add_Applixcation($titre,$description,$file){
+
+            $this->db->set('',$titre);
+            $this->db->set('',$description);
+            $this->db->set('',$file);
+            return $this->db->insert($this->table_application);
+        }
+
+        public function Get_All_Formation(){
+
+            $query = $this->db->select()->get($this->table_formation);
+            return $query;
+        }
+
+        public function Get_All_Application(){
+
+            $query = $this->db->select()->get($this->table_application);
+            return $query;
+        }
+
+        public function Get_One_Formation($id){
+
+            $query = $this->db->where('id_formation',$id)->get($this->table_formation);
+            return $query;
+        }
+
+
+
 
     }
