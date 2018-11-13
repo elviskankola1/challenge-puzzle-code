e9d7aacf49f68accb0dade5eb88c494a87036e21
elvis kankola <elviskankola1@gmail>
2018-11-13 11:51:28 +0200

    UPDATE

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller {
 
 	public function index(){
 
-		$this->load->view('index');
+		$this->load->view('accueil');
 	}
 //=========================================================================================================
 	public function apropos(){
