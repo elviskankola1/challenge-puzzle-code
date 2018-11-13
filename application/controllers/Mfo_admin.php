2701c29711bc70bfb4b8a77c69c442e17a7ce347
elvis kankola <elviskankola1@gmail>
2018-11-13 06:04:20 +0200

    NEW ADN ADDING FIUNCTION

--- application/controllers/Mfo_admin.php
+++ application/controllers/Mfo_admin.php
@@ -1,6 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Mfo_admin extends CI_Controller {
     
+    function __construct(){
+        
+        parent::__construct();
+
+    }
+
+    public function Add_Formation_In_database(){
+
+        $title = strip_tags($this->input->post('titre'));
+        $description = strip_tags($this->input->post('description'));
+        $file = strip_tags($this->input->post('file'));
+        
+    }
 }
