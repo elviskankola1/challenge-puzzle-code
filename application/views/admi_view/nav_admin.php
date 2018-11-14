35f8a9f5ad860fcda999a407981cd5458e81adf2
elviskankola1 <elviskankola1@gmail.com>
2018-11-14 11:08:41 +0200

    update

--- application/views/admi_view/nav_admin.php
+++ application/views/admi_view/nav_admin.php
@@ -60,5 +60,30 @@
 					</li>
 				</ul>
 			</div>
-		</div><!-- /.container-fluid -->
-	</nav>
+		</div>
+    </nav>
+    <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<div class="profile-sidebar">
+			<div class="profile-userpic">
+				<img src="users.png" class="img-responsive" alt="">
+			</div>
+			<div class="profile-usertitle">
+				<div class="profile-usertitle-name">nom admin marque</div>
+				<div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="divider"></div>
+		<form role="search">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Search">
+			</div>
+		</form>
+		<ul class="nav menu">
+			<li class="active"><a href="index.html"><em class="fa fa-dashboard">&nbsp;</em> Tableau de bord</a></li>
+			<li><a href="widgets.html"><em class="fa fa-calendar">&nbsp;</em> Commandes</a></li>
+			<li><a href="charts.html"><em class="fa fa-bar-chart">&nbsp;</em> Statistiques</a></li>
+			
+			<li><a href="login.html"><em class="fa fa-power-off">&nbsp;</em> Deconnexion</a></li>
+		</ul>
+	</div>
