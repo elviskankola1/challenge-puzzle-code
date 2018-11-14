a33a2a3d9f112a91b75dab38fd236f94b97731a5
elviskankola1 <elviskankola1@gmail.com>
2018-11-14 17:16:59 +0200

    new

---
+++ application/views/footer.php
@@ -0,0 +1,70 @@
+<footer id="footer" class="top-space">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Contact</h3>
+						<div class="widget-body">
+							<p>+243 977 756 377<br>
+								<a href="mailto:#">adresse-email.com</a><br>
+								<br>
+								Lubumbashi lomami 19
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">retrouvez nous sur</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons">
+								<a href=""><i class="fa fa-twitter fa-2"></i></a>
+								<a href=""><i class="fa fa-dribbble fa-2"></i></a>
+								<a href=""><i class="fa fa-facebook fa-2"></i></a>
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">Citation</h3>
+						<div class="widget-body">
+							<p></p>
+							<p>
+
+								*Développer et Véhiculer la culture de transparence et de compétence à tous les échelons pour les 
+								intervenants et usagers des comptes selon les normes édictées par l'OHADA révisé et les IFRS
+								* Mettre en place un cadre de concertation et d'échange de connaissances par la Formation continue de
+								de l'Elite des finances (Experts comptables, Etudiants, Cadres des Entreprises, Chercheurs)
+								* Placement de candidats Masters Juniors (Issus du dernier cycle du parcourt académique)
+
+
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="text-right">
+								Copyright &copy; 2018 <a href="mailto:elviskankola1@gmail.com">by Elvis-kankola-<span style="color:green">ourWorld</span></a> 
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+	</footer>	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
