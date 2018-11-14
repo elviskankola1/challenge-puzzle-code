9b80c4b8afd06afc032f539931cde12a34c831f7
elviskankola1 <elviskankola1@gmail.com>
2018-11-14 17:37:09 +0200

    update

--- application/views/about.php
+++ application/views/about.php
@@ -8,17 +8,17 @@
 	<title>SITE</title>
 	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
-	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
-	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-theme.css');?>" media="screen" >
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/main.css');?>">
 </head>
 <body>
 		<div class="navbar navbar-inverse navbar-fixed-top headroom" >
 				<div class="container">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-						<a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Progressus HTML5 template"></a>
+						<a class="navbar-brand" href="index.html"><img src="<?php echo base_url('assets/images/logo.png');?>" alt="Progressus HTML5 template"></a>
 					</div>
 					<div class="navbar-collapse collapse">
 						<ul class="nav navbar-nav pull-right">
