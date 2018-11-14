c0338f76e970d79b008bf5bacee7152cb8cc656f
elviskankola1 <elviskankola1@gmail.com>
2018-11-14 17:37:26 +0200

    update

--- application/views/footer.php
+++ application/views/footer.php
@@ -65,6 +65,6 @@
 	</footer>	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-	<script src="assets/js/headroom.min.js"></script>
-	<script src="assets/js/jQuery.headroom.min.js"></script>
-	<script src="assets/js/template.js"></script>
+	<script src="<?php echo base_url('assets/js/headroom.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/jQuery.headroom.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/template.js');?>"></script>
