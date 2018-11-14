22865b9ec78769c46178f1e93b952f69e5fa8df2
elviskankola1 <elviskankola1@gmail.com>
2018-11-14 17:07:27 +0200

    update

--- application/views/admin_view/nav_admin.php
+++ application/views/admin_view/nav_admin.php
@@ -83,6 +83,7 @@
 			<li class="active"><a href="<?php echo base_url('index.php/mfo_admin');?>"><em class="fa fa-dashboard">&nbsp;</em> Tableau de bord</a></li>
 			<li><a href="<?php echo base_url('index.php/mfo_admin/Page_Add_App');?>"><em class="fa fa-calendar">&nbsp;</em> Publier une application</a></li>
 			<li><a href="<?php echo base_url('index.php/mfo_admin/Page_Add_Form');?>"><em class="fa fa-calendar">&nbsp;</em> Publier une formation</a></li>
+			<li><a href="<?php echo base_url('index.php/mfo_admin/Page_Add_Item_Blog');?>"><em class="fa fa-calendar">&nbsp;</em> Publier une article</a></li>
 			
 			<li><a href="<?php echo base_url('mfo/login');?>"><em class="fa fa-power-off">&nbsp;</em> Deconnexion</a></li>
 		</ul>
