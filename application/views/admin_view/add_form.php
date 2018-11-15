6f5e896841ebc138a5214ff259aba9e08b4ec66a
elviskankola1 <elviskankola1@gmail.com>
2018-11-15 09:46:57 +0200

    update

--- application/views/admin_view/add_form.php
+++ application/views/admin_view/add_form.php
@@ -6,7 +6,7 @@
 			<div class="login-panel panel panel-default" style="width:100%;">
 				<div class="panel-heading">Formulaire d'ajout</div>
 				<div class="panel-body">
-					<?php echo form_open();?>
+					<?php echo form_open_multipart('index.php/mfo_admin/Add_Formation_In_database');?>
                     <div class="form-group">
                                     <div class="input-group"  style="width:60%;">
                                         <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
