263437fa4468027f84c12e3d887ab9fbe3c344c8
elviskankola1 <elviskankola1@gmail.com>
2018-11-15 14:35:53 +0200

    update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
 $autoload['libraries'] = array('database');
 $autoload['drivers'] = array();
-$autoload['helper'] = array('url','form');
+$autoload['helper'] = array('url','form', 'file');
 $autoload['config'] = array();
 $autoload['language'] = array();
 $autoload['model'] = array('main_model','admin_model');
