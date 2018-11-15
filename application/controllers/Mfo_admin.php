4601e615ef79e9b2ffbe5f116415b56317da13bb
elviskankola1 <elviskankola1@gmail.com>
2018-11-15 10:36:43 +0200

    update

--- application/controllers/Mfo_admin.php
+++ application/controllers/Mfo_admin.php
@@ -47,6 +47,9 @@ class Mfo_admin extends CI_Controller {
                 }
             }
             $this->admin_model->Add_Formation($title,$description,$file);
+            $this->load->view('admin_view/header_admin');
+            $this->load->view('admin_view/nav_admin');
+            $this->load->view('admin_view/add_form');
         }
         
     }
