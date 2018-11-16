880479f0579a3322b7e12383d453549abc2e8605
elviskankola1 <elviskankola1@gmail.com>
2018-11-17 00:53:32 +0200

    update

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -47,7 +47,7 @@
 			<div class="row">
 				<h1 class="lead">Vivez la solution en temps reel</h1>
 				<p class="tagline">Notre cabinet est votre solution</p>
-				<p><a class="btn btn-default btn-lg" role="button" href="formation.html">formation</a> 
+				<p><a class="btn btn-default btn-lg" role="button" href="<?php echo base_url('index.php/welcome/formation');?>">formation</a> 
 					<a class="btn btn-action btn-lg" role="button">Logiciel</a></p>
 			</div>
 		</div>
