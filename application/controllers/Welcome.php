52644041032c4bc48955a70413e8ee1470113b70
elviskankola1 <elviskankola1@gmail.com>
2018-11-17 02:51:00 +0200

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 //=========================================================================================================
 	public function souscription_formation(){
 		$id_formation = $this->uri->segment(3);
-		$this->main_model->Get_One_Formatio($id_formation);
+		$data[''] = $this->main_model->Get_One_Formatio($id_formation);
 		$this->load->view('souscription_form');
 	}
 //========================================================================================================
