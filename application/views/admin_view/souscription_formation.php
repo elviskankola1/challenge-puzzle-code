74483c0f0d23d5a9160ec91ccd6c23840f8a321a
elviskankola1 <elviskankola1@gmail.com>
2018-11-17 03:50:54 +0200

    update

--- application/views/admin_view/souscription_formation.php
+++ application/views/admin_view/souscription_formation.php
@@ -3,8 +3,8 @@
 <body style="background-image: url('unique9.jpg'); background-repeat: no-repeat; background-position:center; background-size: 100%;">
 	<div class="row"><br>
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4"  style="width:55%; left:0%;">
-        <? if($msg): ?>
-        <?php foreach($msg as $value):?>
+        <? if($souscription): ?>
+        <?php foreach($souscription as $value):?>
         <div class="login-panel panel panel-default" style="width:100%;">
             <div class="profile-userpic">
 				<img src="<?php echo base_url('assets/images/3.jpg');?>" class="img-responsive" alt="image">
@@ -14,7 +14,7 @@
 				<div class="panel-body">
                     <b>
                         <h5><a href="mailto:<?php echo $value->adresse_user;?>"><?php echo $value->adresse_user;?></a>/<?php echo $value->telephone_user;?></h5>
-                        <p><?php echo $value->message_user?></p>
+                        <p><?php echo $value->formation_user?></p>
                     </b>
                     <hr>
                     <a href="<?php echo base_url('index.php/mfo_admin/delete_message/').$value->id_user;?>">
