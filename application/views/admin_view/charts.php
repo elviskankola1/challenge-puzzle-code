cc2ccf5bc529510109bfffdc4700f6a16249f11b
elviskankola1 <elviskankola1@gmail.com>
2018-11-20 11:27:02 +0200

    update

--- application/views/admin_view/charts.php
+++ application/views/admin_view/charts.php
@@ -3,18 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Lumino - Charts</title>
+	<title>admin</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/datepicker3.css" rel="stylesheet">
 	<link href="css/styles.css" rel="stylesheet">
-	
-	<!--Custom Font-->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-	<!--[if lt IE 9]>
-	<script src="js/html5shiv.js"></script>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
 </head>
 <body style="background-image: url('i5.jpg'); background-repeat: no-repeat; background-position:center; background-size: 100%;">
 	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
