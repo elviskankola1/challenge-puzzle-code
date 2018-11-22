3f14789963ce633a75b65b84ddc5e40763d103fb
elviskankola1 <elviskankola1@gmail.com>
2018-11-22 16:46:28 +0200

    UPDATE

--- application/controllers/Mfo_admin.php
+++ application/controllers/Mfo_admin.php
@@ -210,7 +210,7 @@ class Mfo_admin extends CI_Controller {
             $this->load->view('admin_view/index',$_SESSION);
             $this->load->view('admin_view/footer_admin');
         }else{
-            echo "else";
+            redirect($_SERVER['HTTP_REFERER']);
         }
     }
 
