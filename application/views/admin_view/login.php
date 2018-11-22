b0799ebd64fb479760f640accc64eed13ba1a8a3
elviskankola1 <elviskankola1@gmail.com>
2018-11-22 15:21:33 +0200

    update

--- application/views/admin_view/login.php
+++ application/views/admin_view/login.php
@@ -6,22 +6,23 @@
 			<div class="login-panel panel panel-default" style="width:100%;">
 				<div class="panel-heading">Login admin </div>
 				<div class="panel-body">
-					<?php echo form_open_multipart('index.php/mfo_admin/');?>
+					<?php echo form_open_multipart('index.php/mfo_admin/login_admin');?>
                     <div class="form-group">
-                                    <div class="input-group"  style="width:60%;">
-                                        <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-                                        <input type="text" class="form-control" placeholder="entrez votre login" name="login">
-                                    </div>
-                                </div>
+                        <div class="input-group"  style="width:60%;">
+                            <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
+                                <input type="text" class="form-control" placeholder="entrez votre login" name="login">
+                            </div>
+                        </div>
+                        <div class="input-group"  style="width:60%;">
+                            <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
+                                <input type="text" class="form-control" placeholder="entrez votre mot de passe" name="password">
+                            </div>
+                        </div>
 
-                                <div class="form-group" >
-                                    <div class="input-group">
-                                    <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-                                        <input type="password" class="form-control" placeholder="entrez votre pass" name="password">
-                                    </div>
-                                </div>
-							<button class="btn btn-primary">Publier</button></fieldset>
+                                
+                                <fieldset><button class="btn btn-primary">Publier</button></fieldset>
 					<?php echo form_close();?>
+                    </div>
 				</div>
 			</div>
 		</div>
